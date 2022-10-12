7b31821b0fc1066b3ecb23183a27b5aef32b734d
GiovaniCappelaro <giovani.c@aluno.ifsp.edu.br>
2022-10-12 19:33:49 -0300

    tudo pronto igual do vídeo

---
+++ app/src/main/java/com/giovani/pdm/sendmail/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.giovani.pdm.sendmail
+
+import android.content.Intent
+import android.content.Intent.*
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giovani.pdm.sendmail.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val amb:ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater);
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        //botão de limpar todos os campos
+        amb.cleanBt.setOnClickListener{
+
+            with(amb){
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                messageEt.setText("")
+            }
+            //with(amb) -> todos os q são amb
+        }
+
+        amb.sendBt.setOnClickListener{
+            val sendMailIntent = Intent(ACTION_SENDTO)
+
+            with(sendMailIntent){
+                putExtra(EXTRA_EMAIL, arrayOf(amb.toEt.toString()))
+                putExtra(EXTRA_CC, arrayOf(amb.ccEt.toString()))
+                putExtra(EXTRA_BCC, arrayOf(amb.bccEt.toString()))
+                putExtra(EXTRA_SUBJECT, arrayOf(amb.subjectEt.toString()))
+                putExtra(EXTRA_TEXT, arrayOf(amb.messageEt.toString()))
+
+                type = "message/rfc822"
+                data = Uri.parse("mailto:")
+            }
+
+            val chooserIntent = Intent(ACTION_CHOOSER)
+            chooserIntent.putExtra(EXTRA_INTENT, sendMailIntent)
+            startActivity(chooserIntent)
+        }
+    }
+}
